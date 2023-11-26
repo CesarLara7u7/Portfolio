e237e5a4f1231c5346c836caf8a1dd8cb946e701
Cesar David Lopez Lara <cesar.lopez@qrsof.com>
2023-11-26 16:01:49 -0600

    header and fix console errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,132 +1,139 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
 
-    @ViewChild("nav") nav!: ElementRef
-    @ViewChild("navMenu") navMenu!: HTMLElement
-    @ViewChild("btnToggleNav") btnToggleNav!: HTMLElement
-    workEls = document.querySelectorAll(".work-box");
-    workImgs = document.querySelectorAll(".work-img");
-    @ViewChild("checkBox") checkBox!: HTMLInputElement
-    @ViewChild("yearEl") yearEl!: HTMLElement
-    skillsImages: Array<{ src: string, title: string }> = [
-      {
-        title: "Scala",
-        src: "assets/images/skills/scala.svg"
-      },
-      {
-        title: "Angular",
-        src: "assets/images/skills/angular.svg"
-      },
-      {
-        title: "Akka",
-        src: "assets/images/skills/akka.svg"
-      },
-      {
-        title: "Java",
-        src: "assets/images/skills/java.svg"
-      },
-      {
-        title: "MySQL",
-        src: "assets/images/skills/mysql.svg"
-      },
-      {
-        title: "Postgres",
-        src: "assets/images/skills/postgres.svg"
-      },
-      {
-        title: "Docker",
-        src: "assets/images/skills/docker.svg"
-      },
-      {
-        title: "PHP",
-        src: "assets/images/skills/php.svg"
-      },
-      {
-        title: "Primeng",
-        src: "assets/images/skills/primeng.svg"
-      },
-    ]
-    storedTheme = localStorage.getItem("theme");
+  @ViewChild("nav") nav!: ElementRef
+  @ViewChild("navMenu") navMenu!: HTMLElement
+  @ViewChild("btnToggleNav") btnToggleNav!: HTMLElement
+  workEls = document.querySelectorAll(".work-box");
+  workImgs = document.querySelectorAll(".work-img");
+  @ViewChild("checkBox") checkBox!: HTMLInputElement
+  skillsImages: Array<{ src: string, title: string }> = [
+    {
+      title: "Scala",
+      src: "assets/images/skills/scala.svg"
+    },
+    {
+      title: "Angular",
+      src: "assets/images/skills/angular.svg"
+    },
+    {
+      title: "Akka",
+      src: "assets/images/skills/akka.svg"
+    },
+    {
+      title: "Java",
+      src: "assets/images/skills/java.svg"
+    },
+    {
+      title: "MySQL",
+      src: "assets/images/skills/mysql.svg"
+    },
+    {
+      title: "Postgres",
+      src: "assets/images/skills/postgres.svg"
+    },
+    {
+      title: "Docker",
+      src: "assets/images/skills/docker.svg"
+    },
+    {
+      title: "PHP",
+      src: "assets/images/skills/php.svg"
+    },
+    {
+      title: "Primeng",
+      src: "assets/images/skills/primeng.svg"
+    },
+    {
+      title: "Intellij",
+      src: "assets/images/skills/intellij.svg"
+    },
+  ]
+  storedTheme = localStorage.getItem("theme");
 
-    observer: IntersectionObserver
+  observer: IntersectionObserver
 
-    constructor() {
-        this.initWorkElementsObs()
-        this.initImages()
-        this.observer = new IntersectionObserver(
-            (entries) => {
-                const [entry] = entries;
-                const [textbox, picture] = Array.from(entry.target.children);
-                if (entry.isIntersecting) {
-                    picture.classList.remove("transform");
-                    Array.from(textbox.children).forEach(
-                        (el: any) => (el.style.animationPlayState = "running")
-                    );
-                }
-            },
-            {threshold: 0.3}
-        );
-
-    }
-
-    ngAfterViewInit(): void {
-        this.checkBox.checked = false
-        this.yearEl.textContent = new Date().getFullYear().toString();
-    }
-
-    onToggleNav() {
-        this.toggleNav();
-    }
-
-    toggleNav() {
-        console.log(this.nav.nativeElement.classList)
-        this.nav.nativeElement.classList.toggle("hidden");
-
-        // Prevent screen from scrolling when menu is opened
-        document.body.classList.toggle("lock-screen");
-
-        if (this.nav.nativeElement.classList.contains("hidden")) {
-            this.btnToggleNav.textContent = "menu";
-        } else {
-            // When menu is opened after transition change text respectively
-            setTimeout(() => {
-                this.btnToggleNav.textContent = "close";
-            }, 475);
+  constructor() {
+    this.initWorkElementsObs()
+    this.initImages()
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        const [entry] = entries;
+        const [textbox, picture] = Array.from(entry.target.children);
+        if (entry.isIntersecting) {
+          picture.classList.remove("transform");
+          Array.from(textbox.children).forEach(
+            (el: any) => (el.style.animationPlayState = "running")
+          );
         }
-    }
+      },
+      {threshold: 0.3}
+    );
 
-    onEscape($event: KeyboardEvent) {
-        if ($event.key === "Escape" && !this.nav.nativeElement.classList.contains("hidden")) {
-            this.toggleNav();
-        }
-    }
+  }
 
-    private initWorkElementsObs() {
-        this.workEls.forEach(element => {
-            this.observer.observe((element))
-        })
-    }
+  ngAfterViewInit(): void {
+    this.checkBox.checked = localStorage.getItem("theme") == "dark"
+  }
 
-    private initImages() {
-        this.workImgs.forEach((workImg) => workImg.classList.add("transform"));
-    }
+  onToggleNav() {
+    this.toggleNav();
+  }
 
-    onSwichDarkMode() {
-        if (this.checkBox.checked) {
-            document.body.classList.add("dark");
-            document.body.classList.remove("light");
-            localStorage.setItem("theme", "dark");
-        }else{
-            document.body.classList.remove("dark");
-            document.body.classList.add("light");
-            localStorage.setItem("theme", "light");
-            this.checkBox.checked = false;
-        }
+  toggleNav() {
+    console.log(this.nav.nativeElement.classList)
+    this.nav.nativeElement.classList.toggle("hidden");
+
+    // Prevent screen from scrolling when menu is opened
+    document.body.classList.toggle("lock-screen");
+
+    if (this.nav.nativeElement.classList.contains("hidden")) {
+      this.btnToggleNav.textContent = "menu";
+    } else {
+      // When menu is opened after transition change text respectively
+      setTimeout(() => {
+        this.btnToggleNav.textContent = "close";
+      }, 475);
     }
+  }
+
+  onEscape($event: KeyboardEvent) {
+    if ($event.key === "Escape" && !this.nav.nativeElement.classList.contains("hidden")) {
+      this.toggleNav();
+    }
+  }
+
+  private initWorkElementsObs() {
+    this.workEls.forEach(element => {
+      this.observer.observe((element))
+    })
+  }
+
+  private initImages() {
+    this.workImgs.forEach((workImg) => workImg.classList.add("transform"));
+  }
+
+  onSwitchDarkMode() {
+    this.checkBox.checked = !this.checkBox.checked;
+    if (this.checkBox.checked) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+      localStorage.setItem("theme", "light");
+    }
+    this.currentColor()
+  }
+
+  currentColor() {
+    return this.storedTheme == "dark"? "white" : "black";
+  }
 }
